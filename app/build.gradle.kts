d881d39e867300c462b1fac30350acdd9a626364
martinmarinkovic <martin.marinkovic3@gmail.com>
2026-04-01 21:10:37 +0200

    V3 (1.2) Release - Ads

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.threemdroid.digitalwallet"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
